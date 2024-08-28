6b04b5a11f43a14648e0a6749eae25a084a3352b
ialkeilani <ialkeilani@gmail.com>
2024-08-28 11:28:45 -0400

    admin page mods:   - project header   - field ordering   - field search   - field filtering   - editable grid view of entire model/table

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+
+from django.conf import settings
+# from blog_clone import settings
+
 from . import models
 
 # Register your models here.
@@ -8,9 +12,21 @@ from . import models
 
 @admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
+    # fields = [] # fields and their order when a single record is viewed
     readonly_fields = ('pk',)
+    pass
+
 
 @admin.register(models.Comment)
 class CommentAdmin(admin.ModelAdmin):
     readonly_fields = ('pk',)
+    fields = ["pk", "post", "author", "created_date", "approved_comment", "text",] # fields and their order when a single record is viewed
+    # search_fields = ["pk", "post", "author", "created_date", "approved_comment", "text",]
+    search_fields = ["pk", "post__title", "post__text", "author__username", "created_date", "approved_comment", "text",]
+    list_filter = ["id", "post__id", "author__username", "created_date", "approved_comment",]
+    list_display = ["id", "post", "author", "created_date", "approved_comment"] # grid view of entire table
+    list_editable = ["post", "author", "created_date", "approved_comment"]
+
+
+admin.site.site_header = f"{settings.BASE_DIR.name.replace('_', ' ').title()} administration"
 
